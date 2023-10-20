a27695abee8fd99dc9b60827082f6ff173ca4404
tolossamuel <tolossamuel1@gmail.com>
2023-10-20 10:16:35 +0300

    updated

--- pchar.c
+++ pchar.c
@@ -14,13 +14,13 @@ void f_pchar(stack_t **customStack, unsigned int line_num)
 {
 	if (!customStack || !(*customStack))
 	{
-		fprintf(stderr, "L%d: can't f_pchar, customStack empty\n", line_num);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 		return;
 	}
 	if (isascii((*customStack)->n) == 0)
 	{
-		fprintf(stderr, "L%d: can't f_pchar, value out of range\n", line_num);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_num);
 		exit(EXIT_FAILURE);
 		return;
 	}
