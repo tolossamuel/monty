d690374cceed59919861c214d614d9bdcb79dfb6
tolossamuel <tolossamuel1@gmail.com>
2023-10-20 10:23:54 +0300

    updated

--- print.c
+++ print.c
@@ -24,3 +24,14 @@ unsigned int line_num __attribute__((unused)))
 	}
 	putchar('\n');
 }
+
+/**
+ * pall - Prints elements in the customStack.
+ *
+ * @customStack: The head of the stack.
+ * @line_num: The number of lines.
+ */
+void pall(stack_t **customStack, unsigned int line_num __attribute__((unused)))
+{
+	printStack(*customStack);
+}
