bfc74dd23ed3b0d3f7e46b608ba8d51d093e43eb
tolossamuel <tolossamuel1@gmail.com>
2023-10-20 10:21:35 +0300

    updated

--- pchar.c
+++ pchar.c
@@ -26,3 +26,19 @@ void f_pchar(stack_t **customStack, unsigned int line_num)
 	}
 	printf("%c\n", (*customStack)->n);
 }
+
+/**
+ * pint - Displays the value at the head of the customStack
+ * @customStack: The customStack originating from the main function in start.c
+ * @line_num: The total lines
+ */
+void pint(stack_t **customStack, unsigned int line_num)
+{
+	if (!customStack || !(*customStack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+
+	}
+	printf("%d\n", (*customStack)->n);
+}
